e4397f97ff1b2b4d0e8b83098c86d08844d6e2bd
nmrchvz <npchavez@ualberta.ca>
2021-10-04 16:01:07 -0600

    finished assignment2

--- README.md
+++ README.md
@@ -18,3 +18,15 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Remarks
+========================
+Nomar Chavez
+npchavez
+1603039
+
+This is a forked repo for the purpose of making a web client as a CMPUT 404 assignment.
+I declare that I have not collaborated with anyone on the completion of this assignment
+
+Reference:
+* https://docs.python.org/3/library/urllib.parse.html
+* https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
--- httpclient.py
+++ httpclient.py
@@ -21,36 +21,35 @@
 import sys
 import socket
 import re
-# you may use urllib to encode data appropriately
+# you may use urllib to encode response appropriately
 import urllib.parse
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
 class HTTPResponse(object):
-    def __init__(self, code=200, body=""):
+    def __init__(self, code=200, header = "", body=""):
         self.code = code
+        self.header = header
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
-
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
-    def get_code(self, data):
-        return None
+    def get_code(self, response):
+        return int(response.split()[1])
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self,response):
+        return response.split('\r\n\r\n')[0]
 
-    def get_body(self, data):
-        return None
+    def get_body(self, response):
+        return response.split('\r\n\r\n')[1]
     
-    def sendall(self, data):
-        self.socket.sendall(data.encode('utf-8'))
+    def sendall(self, response):
+        self.socket.sendall(response.encode('utf-8'))
         
     def close(self):
         self.socket.close()
@@ -66,16 +65,99 @@ class HTTPClient(object):
             else:
                 done = not part
         return buffer.decode('utf-8')
+    """
+    Purpose: to properly format a http request
+    Parameters: request_type (str) - the request type (GET/POST)
+                url_parse (str) - the parsed inputted url
+                parameters (str) - the arguments provided for a POST request
+    Return: request (str) - the formatted http request
+    References: https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+    """
+    def request_handler(self, request_type, url_parse, parameters = ""):
+        path = '/'
+        if (url_parse.path != ''):
+            path = url_parse.path
+        request = request_type
+        request += path
+        request += ' HTTP/1.1\r\n'
+        request += 'Host: ' + url_parse.hostname + '\r\n'
 
+        if request_type == "POST ":
+            request += 'Content-Type: application/x-www-form-urlencoded\r\n'
+            if (parameters != None):
+                request += 'Content-Length: ' + str(len(parameters)) + '\r\n'
+            else:
+                request += 'Content-Length: 0\r\n'
+        request += 'Connection: close\r\n\r\n'
+        if (parameters != None):
+            request += parameters
+        return request
+    
+    """
+    Purpose: sends a GET request to a server
+    Parameters: url (str) - inputted url
+                args (str) - None
+    Return: HTTPResponse(code, header, body) - a class that stores the HTTP response
+    References: https://docs.python.org/3/library/urllib.parse.html
+    """
     def GET(self, url, args=None):
         code = 500
         body = ""
-        return HTTPResponse(code, body)
+        port = 80
 
+        url_parse = urllib.parse.urlparse(url)
+
+        if (url_parse.port != None):
+            port = url_parse.port
+
+        self.connect(url_parse.hostname, port)
+
+        self.sendall(self.request_handler("GET ", url_parse, parameters=None))
+        response = self.recvall(self.socket)
+
+        header = self.get_headers(response)
+        code = self.get_code(response)
+        body = self.get_body(response)
+
+        print(code)
+        print(header)
+        print(body)
+
+        self.close()
+        return HTTPResponse(code, header, body)
+    
+    """
+    Purpose: sends a POST request to a server
+    Parameters: url (str) - inputted url
+                args (str) - the arguments provided for a POST request
+    Return: HTTPResponse(code, header, body) - a class that stores the HTTP response
+    References: https://docs.python.org/3/library/urllib.parse.html
+    """
     def POST(self, url, args=None):
         code = 500
         body = ""
-        return HTTPResponse(code, body)
+        parameters = None
+
+        if (args != None):
+            parameters = urllib.parse.urlencode(args)
+
+        url_parse = urllib.parse.urlparse(url)
+
+        self.connect(url_parse.hostname, url_parse.port)
+
+        self.sendall(self.request_handler("POST ", url_parse, parameters))
+        response = self.recvall(self.socket)
+
+        header = self.get_headers(response)
+        code = self.get_code(response)
+        body = self.get_body(response)
+
+        print(code)
+        print(header)
+        print(body)
+        
+        self.close()
+        return HTTPResponse(code, header, body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
